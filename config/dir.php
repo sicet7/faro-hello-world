83862a89ed7c08bd8b67bcb579df53187391674d
Martin René Sørensen <git@sicet7.com>
2021-09-19 23:08:15 +0200

    Made app work in phar files aswell.

--- config/dir.php
+++ config/dir.php
@@ -2,6 +2,14 @@
 
 declare(strict_types=1);
 
+$phar = Phar::running(false);
+
+if ($phar !== '') {
+    return [
+        'root' => dirname($phar),
+    ];
+}
+
 return [
     'root' => dirname(__DIR__),
 ];
